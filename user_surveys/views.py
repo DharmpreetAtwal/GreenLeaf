1eef180a92d9d64b42b6ba853d52cd7020f7a7a2
Ali Asadpoor <aliasadpoor2.1@gmail.com>
2024-11-20 20:51:29 -0500

    updated the user_surveys template and model files

--- user_surveys/views.py
+++ user_surveys/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Survey
+from .models import Survey, Question
 
-# Create your views here.
 def user_surveys(request):
-    surveys = Survey.objects.all()
+    surveys = Survey.objects.all().prefetch_related('question_set')
     template = loader.get_template('user_surveys.html')
-    context = {'surveys': surveys}
+    context = {
+        'surveys': surveys,
+    }
     return HttpResponse(template.render(context, request))
